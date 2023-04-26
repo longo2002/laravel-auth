983d2f34d7491b2ab0586c239daa6b9e0a531631
longo2002 <lombardo.fra2002@gmail.com>
2023-04-26 07:39:03 +0200

    db creation and migration

---
+++ database/migrations/2023_04_26_053645_create_projects.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id("id");
+            $table->string("nome_cliente", 50);
+            $table->text("descrizione_progetto");
+            $table->string("link");
+            $table->string("data_consegna");
+            $table->boolean("completato");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [PageController::class, "homepage"]);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
